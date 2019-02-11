158fb4907280d7903cbad5553c8bfe7e026c0f82
Kyle Shorter <44951956+Kyleshorter@users.noreply.github.com>
2019-02-10 18:20:15 -0600

    Create MainApp.java

---
+++ FileRead/MainApp.java
@@ -0,0 +1,66 @@
+package albumcollection;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainApp {
+
+    private static final String DATABASE_FILE_NAME = "database.txt";
+
+    private static ArrayList<Album> albumCollection = new ArrayList<Album>();
+
+    public static void main(String[] args) {
+
+        readCollectionFromDatabase();
+
+        Album newAlbum = new Album();
+        newAlbum.title = "Blah";
+
+        albumCollection.add(newAlbum);
+
+        writeCollectionToDatabase();
+    }
+
+    private static void writeCollectionToDatabase() {
+
+        try {
+
+            FileWriter fileWriter = new FileWriter(DATABASE_FILE_NAME, false);
+
+            for (Album album : albumCollection) {
+                String title = album.title;
+                fileWriter.write(title + "\n");
+            }
+
+            fileWriter.close();
+
+        } catch (Exception ex) {
+
+            System.out.println("Exception: " + ex);
+        }
+    }
+
+    private static void readCollectionFromDatabase() {
+
+        File file = new File(DATABASE_FILE_NAME);
+
+        try {
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Album album = new Album();
+                album.title = line;
+                albumCollection.add(album);
+            }
+
+        } catch (Exception ex) {
+
+            System.out.println("Exception: " + ex);
+        }
+
+    }
+}
