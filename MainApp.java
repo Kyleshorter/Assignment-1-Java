37f3208debd30a1aeb8faace32f9098b5d50f564
Kyle Shorter <44951956+Kyleshorter@users.noreply.github.com>
2019-02-11 22:24:53 -0600

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -4,19 +4,23 @@ import java.io.*;
 public class MainApp {
 
     static ArrayList<Album> albumCollection = new ArrayList<Album>();
+
     public static void main(String[] args) throws FileNotFoundException {
 
-        Album alb= new Album("thunderstruck", "ac/dc", 1983);
-        System.out.print(alb.toString());
+        //Album alb = new Album("thunderstruck", "ac/dc", 1983);
+        //System.out.print(alb.toString());
 
         File inputFile = new File("database.txt");
         Scanner fin = new Scanner(inputFile);
-        while (fin.hasNextLine()){
+        while (fin.hasNextLine()) {
             String line = fin.nextLine();
-            Album album = new Album();
+            Album album = new Album(line);
+            album.title = line;
+            albumCollection.add(album);
+            System.out.println(album.title);
 
 
+
+            }
         }
-
     }
-}
