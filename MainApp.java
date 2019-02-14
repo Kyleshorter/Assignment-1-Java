65fa2f26a5ba53e7f99f19a71dd13b1e17e42ee3
Kyle Shorter <44951956+Kyleshorter@users.noreply.github.com>
2019-02-13 18:15:04 -0600

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.io.*;
+
+
 public class MainApp {
     private static ArrayList<Album> albumCollection = new ArrayList<Album>();
     public static void main(String[] args) {
@@ -13,17 +17,20 @@ public class MainApp {
 
 private static void readFile(){
     File file = new File("database.txt");
+    int count = 0;
     try{
         Scanner scanner = new Scanner(file);
-        while (scanner.nextLine()) {
+        while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] arr = line.split("~");
             String title = arr[0];
+            System.out.println(count);
             String art = arr[1];
             int year = Integer.parseInt(arr[2]);
             genre gen = genre.values()[Integer.parseInt(arr[3])];
             Album a = new Album(title, art, year);
             albumCollection.add(a);
+            count++;
         }
     } catch (Exception ex) {
         System.out.println("Exception" + ex);
