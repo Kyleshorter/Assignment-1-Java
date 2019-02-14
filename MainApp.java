d42c3765ab89e34dc950b1804a0ff99e51caa468
Kyle Shorter <44951956+Kyleshorter@users.noreply.github.com>
2019-02-13 20:40:17 -0600

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -5,35 +5,46 @@ import java.io.*;
 public class MainApp {
     private static ArrayList<Album> albumCollection = new ArrayList<Album>();
     public static void main(String[] args) {
-
-        Album alb = new Album("Thunderstruck", "ACDC", 1983, genre.ROCK);
-
+        //Album alb = new Album("Thunderstruck", "ACDC", 1983, genre.ROCK);
         //System.out.println(alb.toString());
         readFile();
-       // System.out.println(albumCollection)
-        add();
-        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Display list of all albums in the collection");
+        System.out.println("2. Add a new album to the collection");
+        System.out.println("3. Search for an album given the name of the artist or title, or part of the album or name");
+        System.out.println("4. Delete an album from the collection");
+        System.out.println("5. Exit Program");
+       while(true) {
+           int choice = input.nextInt();
+
+           if (choice == 1) {
+               System.out.println(albumCollection.toString());
+           } else if (choice == 2) {
+               add();
+           } else if (choice == 5) {
+               System.exit(0);
+           }
+       }
+    }
 
 
-private static void readFile(){
-    File file = new File("database.txt");
-    int count = 0;
-    System.out.println(count);
-    try{
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            System.out.println(count);
-            String line = scanner.nextLine();
-            String[] arr = line.split("~");
-            String title = arr[0];
-            String art = arr[1];
-            int year = Integer.parseInt(arr[2]);
-            genre gen = genre.values()[Integer.parseInt(arr[3])];
-            Album a = new Album(title, art, year, gen);
-            albumCollection.add(a);
-            count++;
-        }
-    } catch (Exception ex) {
+    private static void readFile(){
+        File file = new File("database.txt");
+        try{
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] arr = line.split("~");
+                String art = arr[0];
+                String title = arr[1];
+                int year = Integer.parseInt(arr[2]);
+                genre gen = genre.values()[Integer.parseInt(arr[3])];
+                Album a = new Album(title, art, year, gen);
+                albumCollection.add(a);
+            }
+        } catch (Exception ex) {
+
         System.out.println("Exception" + ex);
     }
 }
@@ -71,6 +82,5 @@ private static void add(){
         else{
             System.out.println("Error: invalid input");
         }
-        System.out.println(albumCollection.toString());
 }
 }
