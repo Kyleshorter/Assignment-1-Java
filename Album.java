734d5478b0f1d9719191d4a175e82504e232ab6c
Kyle Shorter <44951956+Kyleshorter@users.noreply.github.com>
2019-02-14 22:27:07 -0600

    Update Album.java

--- Album.java
+++ Album.java
@@ -1,36 +1,61 @@
-enum genre{
-    ROCK, RAP, COUNTRY, POP;
+enum genre {
+    RAP, ROCK, COUNTRY, POP
 }
+/**
+ * This class initialize a class named Album and 4 different instance variables for the title, artist, release year, and
+ * the genre.
+ */
 public class Album {
-    private String title;
-    private String artist;
-    private int releaseYear;
-    private genre genre;
 
-    public Album(String title, String artist, int releaseYear){
-        this.title = title;
+    public String Title;
+    public String artist;
+    public int releaseYear;
+    public genre genre;
+
+    public Album(String Title, String artist, int releaseYear, genre genre) {
+        this.Title = Title;
         this.artist = artist;
         this.releaseYear = releaseYear;
-        this.genre = null;
-    }
-    public void setGenre(genre genre){
         this.genre = genre;
     }
-    public String getTitle(){
-        return title;
+    /**
+     * creates a getter for the title.
+     *
+     * @return an <CODE>Title</CODE>  for the getter.
+     */
+    public String getTitle() {
+        return Title;
     }
-    public String getArtist(){
+    /**
+     * creates a getter for the artist.
+     *
+     * @return an <CODE>artist</CODE>  for the getter.
+     */
+    public String getArtist() {
         return artist;
     }
-    public int getReleaseYear() {
+    /**
+     * creates a getter for the release year.
+     *
+     * @return an <CODE>release year</CODE>  for the getter.
+     */
+    public int getReleaseYear(){
         return releaseYear;
     }
-    public genre getGenre(){
+    /**
+     * creates a getter for the genre.
+     *
+     * @return an <CODE>genre</CODE>  for the getter.
+     */
+    public genre getGenre() {
         return genre;
     }
-    @Override
-    public String toString(){
-        return "This album is " + this.title + " The artist is " + this.artist + "The release year was " + this.releaseYear +
-                "The genre is " + this.genre;
+    /**
+     * Creates a toString method that allows it to display.
+     *
+     * @return an <CODE>Title</CODE> , <CODE>artist</CODE> , <CODE>release year</CODE> , <CODE>genre</CODE> .
+     */
+    public String toString() {
+        return "\n" + "Title: " + this.Title + " Artist: " + this.artist + " ReleaseYear: " + this.releaseYear + " Genre: " + this.genre + "\n" ;
     }
 }
